d69b4f159ef01c43f5f213e764536358526d3c85
mzhn727 <45910800+mzhn727@users.noreply.github.com>
2021-01-02 15:59:22 +0900

    to secction4-19

--- src/index.js
+++ src/index.js
@@ -1,10 +1,66 @@
-import "./styles.css";
+/**
+ *  const,let などの宣言
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// var val1 = "var";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "uwagaki";
+// console.log(val1);
+
+// // var変数は再宣言可能
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "lethensu";
+// console.log(val2);
+
+// // let変数は上書き可能
+// val2 = "lethensu uwagaki";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "letsaisenngen";
+
+// const val3 = "const hensu";
+// console.log(val3);
+
+//const 上書き不可能
+// val3 = "uwagaki";
+
+// const val3 = "const saisengen";
+
+// constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "test",
+//   age: 31
+// };
+// val4.name = "aaaa";
+// val4.address = "oita";
+// console.log(val4);
+
+// constで宣言した配列の中身は変えられる。追加もできる
+// constで定義した配列はプロパティの変更が可能
+
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+
+const name = "mizuho";
+const age = "31";
+
+//私の名前はMIZUHOです。年齢は31歳です。と表示したいとき
+
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です";
+console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です!年齢は${age}です`;
+console.log(message2);
